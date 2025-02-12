a124ec3a24c2fcc311f1d2471462a5b73ef0e537
Yuli <yulia.yurchenko90@gmail.com>
2025-02-12 18:05:56 +0100

    validate recipes

--- src/services/RecipesStorage.ts
+++ src/services/RecipesStorage.ts
@@ -1,24 +1,27 @@
 import { collection, getDoc, getDocs, doc, addDoc } from "firebase/firestore"
 import { db } from "./firestore.ts"
 import { Recipe, RecipeRequest } from "../assets/types.ts"
+import { validateRecipe } from "./validator.ts"
 
 const collectionName = "Recipe"
 
 export const getRecipes = async (): Promise<Recipe[]> => {
   const recipesCollection = collection(db, collectionName)
   const result = await getDocs(recipesCollection)
-  return result.docs.map((doc) => {
-    const data = doc.data()
-    return {
-      id: doc.id,
-      title: data.title,
-      ingredients: data.ingredients,
-      image: data.image,
-      instructions: data.instructions,
-      difficulty: data.difficulty,
-      duration: data.duration,
-    }
-  })
+  return result.docs
+    .map((doc) => {
+      const data = doc.data()
+      return {
+        id: doc.id,
+        title: data.title,
+        ingredients: data.ingredients,
+        image: data.image,
+        instructions: data.instructions,
+        difficulty: data.difficulty,
+        duration: data.duration,
+      } as Recipe
+    })
+    .filter((recipe) => validateRecipe(recipe))
 }
 
 export const getRecipeById = async (id: string): Promise<Recipe | null> => {
